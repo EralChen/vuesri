974c97e108c57838b7048d3e363542f76dbf8d4f
Eral Chen <eralchen@foxmail.com>
2021-11-24 10:02:37 +0800

    vitepress init

---
+++ press/.vitepress/config/index.ts
@@ -0,0 +1,6 @@
+import type {UserConfig} from 'vitepress'
+import {LIB_NAME} from '../../../config/project'
+export const config:UserConfig = {
+  title: LIB_NAME,
+  description: 'Just playing around.',
+}
