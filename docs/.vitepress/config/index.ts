f465ce768b6a6367a5a8836ca8023f3996d963c0
Eral Chen <eralchen@foxmail.com>
2021-12-29 13:35:55 +0800

    docs

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -2,5 +2,6 @@ import type {UserConfig} from 'vitepress'
 import {LIB_NAME} from '../../../config/project'
 export const config:UserConfig = {
   title: LIB_NAME,
+  base: '/docs/',
   description: 'Just playing around.',
 }
---
+++ docs/index.md
@@ -0,0 +1,7 @@
+---
+home: true
+heroText: Hero Title
+tagline: Hero subtitle
+actionText: Get Started
+actionLink: /guide/
+---
