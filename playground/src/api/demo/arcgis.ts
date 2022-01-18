48ad716387273f65772a841ba92b7a9a5a990036
Eral Chen <eralchen@foxmail.com>
2022-01-18 17:10:50 +0800

    chart

--- playground/src/api/demo/arcgis.ts
+++ playground/src/api/demo/arcgis.ts
@@ -1,4 +1,5 @@
 import request from '../_request/arcgis'
+import {FeatureCollection, Polygon} from 'geojson'
 export function getTestData () {
   return request({
     method: 'GET',
@@ -7,6 +8,22 @@ export function getTestData () {
     },
     url: '/test_point2/MapServer/0/query',
   }).then(res => {
+    return res
+  })
+}
+
+export function getBoundry () {
+  return request<FeatureCollection<Polygon, {
+    name: string,
+    f_name: string,
+  }>>({
+    method: 'GET',
+    baseURL: 'http://116.63.63.191:6080/arcgis/rest/services',
+    url: '/FGW/nb_xzqh/MapServer/0/query',
+  }).then(res => {
+    res.features.forEach(item => {
+      item.properties.name = item.properties.f_name
+    })
     return res
   })
 }
--- playground/src/views/_demo-echart/geo-3d/chart-config.vue
+++ playground/src/views/_demo-echart/geo-3d/chart-config.vue
@@ -1,10 +1,21 @@
 <script lang="ts">
 import { ECharts, EChartsOption } from 'echarts'
-import { defineComponent, inject, nextTick } from 'vue'
+import { defineComponent, inject, nextTick, onMounted } from 'vue'
 import chinaGeoJson from './chinaGeo.json'
+import usaGeoJson from './usaGeo.json'
 import * as echarts from 'echarts'
 import 'echarts-gl'
-echarts.registerMap('china', {geoJSON: chinaGeoJson } as any)
+import { getBoundry } from '@/api/demo/arcgis'
+import { FeatureCollection } from 'geojson'
+import wealth from './wealth.json'
+const wealthMap = wealth.reduce((a, c) => {
+  return (a[c.name] = c.wealth, a)
+}, {} as Record<string, number>)
+function registerMap (name: string, data: any) {
+  if (!echarts.getMap(name)) {
+    echarts.registerMap(name, {geoJSON: data } as any)
+  }
+}
 function getRandomColor (){
   const r = Math.floor(Math.random() * 255)
   const g = Math.floor(Math.random() * 255)
@@ -16,39 +27,77 @@ export default defineComponent({
   setup () {
     const echart = inject<ECharts>('echart')
     if (!echart) throw new Error()
-    const options = {
-      // geo3D: {
-
-      // },
-      series: [
-        {
-          type: 'map3D',
-          map: 'china',
-          viewControl: {
-            distance: 50,
-            panMouseButton: 'left',
-            rotateMouseButton: 'right',
-          },
-
-          data: chinaGeoJson.features.map(item => {
-            return {
-              name: item.properties.name,
-              label: {
-                show: true,
-                color: getRandomColor(),
-              },
-              itemStyle: {
-          
-                color: getRandomColor(),
-              },
-            }
-          }),
+    const itemConfig = {
+      emphasis: {
+        label: { 
+          show: true,
+          color: 'red',
         },
-      ],
-    } as EChartsOption
-    nextTick(() => {
-      echart.setOption(options)
+      },
+      // itemStyle: {
+      //   color: getRandomColor(),
+      // },
+    }
+    onMounted(async () => {
+      const nbjson = await getBoundry()
+      registerMap('宁波市', nbjson)
+      const options = {
+        tooltip: {
+          trigger: 'item',
+          formatter: '{b}<br/>{c} (p / km2)',
+        },
+        visualMap: {
+          type: 'continuous',
+          min: 0,
+          max: 45180,
+          text: ['High', 'Low'],
+          realtime: false,
+          calculable: true,
+          inRange: {
+            color: ['lightskyblue', 'yellow', 'orangered'],
+          },
+        },
+        
+        series: [
+          {
+            type: 'map3D',
+            map: '宁波市',
+            viewControl: {
+              distance: 120,
+              panMouseButton: 'left',
+              rotateMouseButton: 'right',
+              alpha: 60,
+            },
+
+            data: nbjson.features.map(item => {
+              return {
+                name: item.properties.name,
+                value: wealthMap[item.properties.name] || 0,
+              
+                ...itemConfig,
+
+              }
+            }),
+          },
+        ],
+      } as EChartsOption
+      nextTick(() => {
+        echart.setOption(options)
+      })
     })
+
+    echart.on('click', function (params) {
+      const data = params.data as { name: string }
+      // 
+      console.log(data) 
+      registerMap('鄞州区', usaGeoJson)
+      echart.setOption({
+        series: [
+          { map: '鄞州区' },
+        ],
+      })
+    })
+
     return {}
   },
 })
