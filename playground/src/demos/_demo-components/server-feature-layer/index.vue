f23012c6155756e1cc9c89c97723c5882cdbcb11
Eral Chen <eralchen@foxmail.com>
2022-04-08 14:21:37 +0800

    ServerFeatureLayer

--- playground/src/demos/_demo-components/server-feature-layer/index.vue
+++ playground/src/demos/_demo-components/server-feature-layer/index.vue
@@ -1,13 +1,36 @@
 <script lang="ts" setup>
 import { VaMapView } from '@vuesri/components/map-view'
 import { VaSkyBasemap } from '@vuesri/components/sky-basemap'
-import { VaServerFeatureLayer } from '@vuesri/components/server-feature-layer'
+import { VaServerFeatureLayer, __VaServerFeatureLayer } from '@vuesri/components/server-feature-layer'
+import { SimpleRenderer } from 'esri/renderers'
+import { SimpleMarkerSymbol } from 'esri/symbols'
+const layerLoad:__VaServerFeatureLayer.OnLoad = async (e) => {
+  const view = e.view as __esri.MapView
+  await e.layer.when()
+  view.goTo(e.layer.fullExtent)
+}
+const layerClick:__VaServerFeatureLayer.OnClick = async (e) => {
+  const { result } = e
+  if (result) {
+    console.log(result)
+  }
+}
+const renderer = new SimpleRenderer({
+  symbol: new SimpleMarkerSymbol({
+    size: 10,
+    color: 'green',
+  }),
+})
 </script>
 <template>
 <VaMapView>
   <VaSkyBasemap></VaSkyBasemap>
   <VaServerFeatureLayer 
+    :renderer="renderer"
+    :cursor="'pointer'"
     :url="'http://116.63.63.191:6080/arcgis/rest/services/test_server/test_point2/MapServer'"
+    @load="layerLoad"
+    @click="layerClick"
   >
   </VaServerFeatureLayer>
 </VaMapView>
