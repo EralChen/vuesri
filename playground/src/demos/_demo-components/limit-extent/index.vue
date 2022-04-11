70533ac054ef592beb34d2abc71daf71261634d4
Eral Chen <eralchen@foxmail.com>
2022-04-11 09:18:55 +0800

    limit test

--- playground/src/demos/_demo-components/limit-extent/index.vue
+++ playground/src/demos/_demo-components/limit-extent/index.vue
@@ -4,10 +4,14 @@ import { VaSkyBasemap } from '@vuesri/components/sky-basemap'
 import { VaLimitExtent } from '@vuesri/components/limit-extent'
 import Extent from 'esri/geometry/Extent'
 const limitExtent = new Extent({
-  xmax: 121.5041884991781,
-  xmin: 121.19622979659954,
-  ymax: 29.874038474025863,
-  ymin: 29.70256926323602,
+  // xmax: 121.5041884991781,
+  // xmin: 121.19622979659954,
+  // ymax: 29.874038474025863,
+  // ymin: 29.70256926323602,
+  xmax: 9999,
+  xmin: -9999,
+  ymax: 90,
+  ymin: -90,
 })
 </script>
 <template>
--- playground/src/demos/_demo-components/map-view/index.vue
+++ playground/src/demos/_demo-components/map-view/index.vue
@@ -1,9 +1,22 @@
 <script lang="ts" setup>
 import { VaMapView } from '@vuesri/components/map-view'
 import { VaSkyBasemap } from '@vuesri/components/sky-basemap'
+import { lods } from '@vuesri/shared/config/tile-info/4490'
+import { Extent, Polygon } from 'esri/geometry'
+const defaultOptions:__esri.MapViewProperties = {
+  constraints: {
+    lods: lods.slice(1, 18) as any,
+    geometry: new Extent({
+      xmax: 203.34946600898905,
+      xmin: -136.93324043399093,
+      ymax: 88.13649929457745,
+      ymin: -87.44797255422543,
+    }),
+  },
+}
 </script>
 <template>
-  <VaMapView>
+  <VaMapView :default-options="defaultOptions">
     <VaSkyBasemap></VaSkyBasemap>
   </VaMapView>
 </template>
