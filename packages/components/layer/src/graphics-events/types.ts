d786115cbfbe69f92dd0e94cd152822d95598136
Eral Chen <eralchen@foxmail.com>
2022-04-13 17:22:58 +0800

    types

--- packages/components/layer/src/graphics-events/types.ts
+++ packages/components/layer/src/graphics-events/types.ts
@@ -1,6 +1,10 @@
 import { ReturnVoid } from 'vunk/shared/types'
 import { ClickEvent, PointerMoveEvent } from './view/types'
 
+export {
+  ClickEvent, PointerMoveEvent,
+}
+
 // handler
 export type OnClick = (e: ClickEvent) => ReturnVoid
 export type OnPointerMove = (e: PointerMoveEvent) => ReturnVoid
