2559a036c7c03c5f2f164782e2b0ebfb418da405
Eral Chen <eralchen@foxmail.com>
2022-04-15 18:26:48 +0800

    gen types.js

--- packages/components/gulpfile.ts
+++ packages/components/gulpfile.ts
@@ -38,7 +38,7 @@ export default parallel(
       cwd: path.resolve(__dirname, './'),
       onlyFiles: true,
       absolute: true,
-      ignore: [...filePathIgnore, '**/types.ts'],
+      ignore: [...filePathIgnore],
     })
 
     filePaths.forEach(item => {
