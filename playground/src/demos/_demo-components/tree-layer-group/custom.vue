e84a7b8495a3e827c94fed65b9c4c76204b2ba86
EralChen <eralchen@foxmail.com>
2022-08-19 13:51:50 +0800

    change

---
+++ playground/src/demos/_demo-components/tree-layer-group/custom.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import { defineComponent, watch } from 'vue'
+
+export default defineComponent({
+  props: {
+    name: {
+      type: String,
+    },
+  },
+  setup (props) {
+    console.log(props.name)
+    watch(() => props.name, () => {
+      console.log(props.name)
+    })
+    return {}
+  },
+})
+</script>
+<template>
+  <div>111 {{ name }}</div>
+</template>
